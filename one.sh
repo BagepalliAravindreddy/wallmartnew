29d7fd88534b94ed74b61bf0540b72f087109691
BagepalliAravindreedy <aravindreddybagepalli@gmail.com>
2021-07-22 12:00:56 +0530

    updated for revert purpose

--- one.sh
+++ one.sh
@@ -1,4 +1,5 @@
  HI this is aravind reddy 
  IM FROM bangalore
- looking for a devops Oppurtinty to prove my skills 
+ looking for a devops Oppurtinty to prove my skills
 
+ this iam changing for the revert purpose.
